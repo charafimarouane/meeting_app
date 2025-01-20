109b72b3e5b32f10185391f785c730da9e11c1ee
charafimarouane <charafimarouane007@gmail.com>
2025-01-20 22:41:02 +0100

    adding mobile nav and fix sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-4 py-4'>
+      <Link 
+        href={"/"}
+        className='flex items-center gap-1'
+      >
+        <Image 
+          src="/vercel.svg"
+          width={32}
+          height={32}
+          alt='Logo'
+          className='max-sm:size-10'
+        />
+        <p className='text-[24px] text-white font-extrabold max-sm:hidden'>Yoom</p>
+      </Link>
+      <div className='flex-between gap-5'>
+      <MobileNav/>
+      </div>
+    </nav>
   )
 }
 
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,11 +14,7 @@ const Sidebar = () => {
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
         <div className='flex flex- flex-col gap-6'>
             {sidebarLinks.map((link) => {
-                const isActive = pathname === link.route 
-                console.log(pathname);
-                console.log(link.route);
-                
-                
+                const isActive = pathname === link.route                 
                 return(
                     <Link
                         href={link.route}
